fab40dc4893a66fb96774b339ee5becbedd01bf6
Mikhail <newlevelcenter@gmail.com>
2021-09-19 18:06:54 +0300

    day03

---
+++ messanges/old/tasks/python-ta-commands-if-06.py
@@ -0,0 +1,13 @@
+string_in = input("Input message, please: ")
+if string_in == '/rule1':  # if 
+    string_out = 'Consider minor expenses'  #string_out =
+elif string_in == '/rule2':  # if 
+    string_out = 'Set long-term financial goals'  #string_out =
+elif string_in == '/rule3':  # if 
+    string_out = 'Upgrade your professional skills'  #string_out =
+elif string_in == '/rule4':  # if 
+    string_out = 'Live like an investor'  #string_out =
+else:
+    string_out = string_in
+
+print(string_out)
